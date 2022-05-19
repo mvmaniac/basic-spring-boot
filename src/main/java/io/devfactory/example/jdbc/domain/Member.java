e86e6ebb5f6158b5823bb0df85c1fc92aaf10e11
yhs <mvmaniaz@gmail.com>
2022-05-19 20:07:27 +0900

    [추가] JDBC 이해 완료

---
+++ src/main/java/io/devfactory/example/jdbc/domain/Member.java
@@ -0,0 +1,33 @@
+package io.devfactory.example.jdbc.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+public class Member {
+
+  private String memberId;
+  private int money;
+
+  public Member(String memberId, int money) {
+    this.memberId = memberId;
+    this.money = money;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof Member member)) return false;
+    if (money != member.money) return false;
+    return Objects.equals(memberId, member.memberId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(memberId, money);
+  }
+
+}
