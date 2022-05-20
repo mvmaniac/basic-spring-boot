632a30443308d6c31d76f47ef56836a1689b7886
yhs <mvmaniaz@gmail.com>
2022-05-20 23:10:41 +0900

    [추가] 커넥션풀과 데이터소스 이해 완료

---
+++ src/test/java/io/devfactory/example/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,61 @@
+package io.devfactory.example.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import static io.devfactory.example.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+class ConnectionTest {
+
+  @DisplayName("DriverManager 테스트")
+  @Test
+  void driverManager() throws SQLException {
+    Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+    log.info("connection={}, class={}", connection1, connection1.getClass());
+    log.info("connection={}, class={}", connection2, connection2.getClass());
+  }
+
+  @DisplayName("DriverManagerDataSource 테스트")
+  @Test
+  void driverManagerDataSource() throws SQLException {
+    // 항상 새로운 커넥션을 획득
+    DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    useDataSource(dataSource);
+  }
+
+  @DisplayName("HikariDataSource 테스트")
+  @Test
+  void dataSourceConnectionPool() throws SQLException, InterruptedException {
+    // 커넥션 풀링
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(URL);
+    dataSource.setUsername(USERNAME);
+    dataSource.setPassword(PASSWORD);
+    dataSource.setPoolName("MyPool");
+    dataSource.setMaximumPoolSize(10); // 기본값 10임...
+
+    useDataSource(dataSource);
+    TimeUnit.SECONDS.sleep(1);
+  }
+
+  private void useDataSource(DataSource dataSource) throws SQLException {
+    Connection connection1 = dataSource.getConnection();
+    Connection connection2 = dataSource.getConnection();
+
+    log.info("useDataSource - connection={}, class={}", connection1, connection1.getClass());
+    log.info("useDataSource - connection={}, class={}", connection2, connection2.getClass());
+  }
+
+}
